e6dfb492ac76b33e10f8adcbbd73726544d59b1f
Vaibhav Shelke <vaibhavshelke017@gmail.com>
2018-04-14 02:44:38 +0530

    fixed udp write issue

--- controller/udp/udp.cpp
+++ controller/udp/udp.cpp
@@ -1,31 +1,39 @@
 #include "udp.h"
 
 Udp::Udp(int port) {
-    addrlen = sizeof(remaddr);
-    memset((char *)&remaddr, 0, sizeof(remaddr)); 
-    memset((char *)&myaddr, 0, sizeof(myaddr));
-    myaddr.sin_family = AF_INET;
-    myaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    myaddr.sin_port = htons(port);
+    addrlen = sizeof(ground);
+    memset((char *)&rover, 0, sizeof(rover));
+    rover.sin_family = AF_INET;
+    rover.sin_addr.s_addr = htonl(INADDR_ANY);
+    rover.sin_port = htons(port);
     if ((sock = socket(PF_INET, SOCK_DGRAM, 0)) < 0) {
         cout << "ERROR: setting socket in UDP" << endl;
     }
-    if ((bnd = bind(sock, (struct sockaddr *)&myaddr, sizeof(myaddr))) < 0) {
+    if ((bnd = bind(sock, (struct sockaddr *)&rover, sizeof(rover))) < 0) {
         cout << "ERROR: binding server in UDP" << endl;
     }
 }
 
-unsigned char* Udp::read() {  
-    if ( recvfrom(sock, buffer, BUFSIZE, 0, (struct sockaddr *)&remaddr, &addrlen) > 0) {
+unsigned char* Udp::read() {
+    static struct timeval stTimeOut;
+    stTimeOut.tv_sec = TIMEOUT;
+    stTimeOut.tv_usec = U_TIMEOUT;
+    fd_set stReadFDS;
+    FD_ZERO(&stReadFDS);
+    FD_SET(sock, &stReadFDS);
+    if (select(sock + 1, &stReadFDS, NULL, NULL, &stTimeOut) < 0) {
+        cout << "ERROR: setting timeout function in UDP" << endl;
+    }
+    if (FD_ISSET(sock, &stReadFDS) && 
+        recvfrom(sock, buffer, R_BUFSIZE, 0, (struct sockaddr *)&ground, &ground) > 0) {
         return buffer;
     } else {
         return (unsigned char*)"NULL";
     }
 }
 
-int Udp::write(unsigned char *data) {
-    remaddr.sin_port = htons(23907);
-    cout << remaddr.sin_port;
-    return sendto(sock, data, sizeof(data), 0, (struct sockaddr*)&remaddr, sizeof(remaddr));
+int Udp::write(unsigned char *data, int port) {
+    ground.sin_port = htons(port);
+    return sendto(sock, data, W_BUFSIZE, 0, (struct sockaddr*)&ground, sizeof(ground));
 }
 
